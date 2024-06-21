47ded0b3971df541860af2972c4cbabfcd74f6de
Alexisjessy <alexislacroix33@gmail.com>
2024-06-21 15:22:58 +0700

    Exercice commit 21-06-2024

--- src/main/java/fr/afpa/employees/Employee.java
+++ src/main/java/fr/afpa/employees/Employee.java
@@ -1,39 +1,165 @@
 package fr.afpa.employees;
 
-/*
-Objectif :
-L’objectif de ce TP est de concevoir un programme en console basé sur une approche objet et
-permettant de gérer (vraiment très basiquement) des salariés d’une entreprise.
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 
-Vous allez écrire une classe représentant les salariés d'une entreprise.
-Vous pourrez vous aider du PDF disponible sur pour obtenir des informations sur la façon de faire.
-*/
 
-class Employee
-{
+/**
+ * Objectif :
+ * L’objectif de ce TP est de concevoir un programme en console basé sur une
+ * approche objet et
+ * permettant de gérer (vraiment très basiquement) des salariés d’une
+ * entreprise.
+ * 
+ * Vous allez écrire une classe représentant les salariés d'une entreprise.
+ * Vous pourrez vous aider du PDF disponible pour obtenir des informations sur
+ * la façon de faire.
+ */
+
+public class Employee {
+
 	/**
 	 * Matricule de l'employé
 	 */
 	private String registrationNumber;
 
-	// TODO compléter les attributs comme présenté dans le PDF
+	
+	private String lastName;
+	private String firstName;
+	private LocalDate birthDate;
+	private double salary;
+	private final int socialRate = 30;
 
-	// TODO compléter le constructeur de la classe
-	public Employee(String registrationNumber) {
-		this.registrationNumber = registrationNumber;
+	
+	private static ArrayList<Employee> employeeList = new ArrayList<>();
+
+	
+	public Employee(String registrationNumber, String lastName, String firstName, String birthDay, double salary) {
+		setRegistrationNumber(registrationNumber);
+		setLastName(lastName);
+		setFirstName(firstName);
+		setBirthDate(birthDay);
+		this.salary = salary;
+
+	
+		employeeList.add(this);
 	}
 
-	// TODO implémenter les setters et getters de la classe (permet d'accéder aux attributs privés)
+	// Getters et Setters
+
 	public String getRegistrationNumber() {
 		return registrationNumber;
 	}
-
-	public void setRegistrationNumber(String registrationNumber) throws Exception {
-		this.registrationNumber = registrationNumber;
+	public void setRegistrationNumber(String registrationNumber) {
+        if (!isValidRegistrationNumber(registrationNumber)) {
+            throw new IllegalArgumentException("Le format du matricule est invalide. Il doit être de la forme nnXXXnn.");
+        }
+        this.registrationNumber = registrationNumber;
+    }
+	public String getLastName() {
+		return lastName;
 	}
 
-	// TODO ajouter les getters et les setters
+	public void setLastName(String lastName) {
+		if (lastName == null || lastName.trim().isEmpty() || containsDigit(lastName)) {
+			throw new IllegalArgumentException("Le nom ne peut pas être vide et ne doit pas contenir de chiffres.");
+		}
+		this.lastName = lastName;
+	}
 
-	// TODO implémenter la méthode "toString()" qui renvoie une chaîne de caractère qui représente un objet de la classe employé
-	// plus d'information sur la méthode "toString()" -> https://codegym.cc/fr/groups/posts/fr.986.mthode-java-tostring
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		if (firstName == null || firstName.trim().isEmpty() || containsDigit(firstName)) {
+			throw new IllegalArgumentException("Le prénom ne peut pas être vide et ne doit pas contenir de chiffres.");
+		}
+		this.firstName = firstName;
+	}
+
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(String birthDay) {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		try {
+			this.birthDate = LocalDate.parse(birthDay, formatter);
+		} catch (DateTimeParseException e) {
+			throw new IllegalArgumentException("Entrez la date de naissance au format yyyy-MM-dd.");
+		}
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public int getSocialRate() {
+		return socialRate;
+	}
+
+	// Méthode toString pour représenter un objet de la classe Employee sous forme
+	// de chaîne de caractères
+	@Override
+	public String toString() {
+		return "Employee{" +
+				"registrationNumber='" + getRegistrationNumber() + '\'' +
+				", lastName='" + getLastName() + '\'' +
+				", firstName='" + getFirstName() + '\'' +
+				", birthDate=" + getBirthDate() +
+				", salary=" + getSalary() +
+				", socialRate=" + getSocialRate() +
+				'}';
+	}
+
+	// Méthode pour calculer le salaire net
+	public double netSalary() {
+		return this.salary - this.salary * (this.socialRate / 100.0);
+	}
+
+	// Méthode pour calculer le nombre de jours avant le prochain anniversaire
+	public long daysBeforeBirthday() {
+		LocalDate currentDate = LocalDate.now();
+		LocalDate nextBirthday = this.birthDate.withYear(currentDate.getYear());
+
+		// Si l'anniversaire est déjà passé cette année, prendre l'année suivante
+		if (nextBirthday.isBefore(currentDate) || nextBirthday.isEqual(currentDate)) {
+			nextBirthday = nextBirthday.plusYears(1);
+		}
+
+		return ChronoUnit.DAYS.between(currentDate, nextBirthday);
+	}
+
+	// Méthode pour vérifier si une chaîne contient des chiffres
+
+
+	private boolean containsDigit(String str) {
+		for (char c : str.toCharArray()) {
+			if (Character.isDigit(c)) {
+				return true;
+			}
+		}
+		return false;
+	}
+    
+	   // Méthode pour vérifier le format du matricule
+	   private boolean isValidRegistrationNumber(String registrationNumber) {
+        return registrationNumber.matches("\\d{2}[A-Za-z]{3}\\d{2}");
+    }
+
+	// Méthode pour afficher les informations de tous les employés
+	public static void displayAllEmployees() {
+		for (Employee employee : employeeList) {
+			System.out.println(employee);
+		}
+	}
+
 }
--- src/main/java/fr/afpa/employees/EmployeeMain.java
+++ src/main/java/fr/afpa/employees/EmployeeMain.java
@@ -1,21 +1,33 @@
 package fr.afpa.employees;
 
 import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
 
 /**
  * Classe principale du projet, contient la fonction "main"
  */
-class EmployeeMain
-{
-	public static void main(String[] args) 
-	{
-		System.out.println("\n----- Exercice de programmation objet - classe \"Employee\" -----");
+public class EmployeeMain {
+    public static void main(String[] args) {
+        System.out.println("\n----- Exercice de programmation objet - classe \"Employee\" -----");
 
-		// TODO instanciez 4 objets de la classe "Employee" (une fois celle-ci implémentée)
-		Employee employee1 = new Employee("11ABC22");
+        // Instanciation de 4 objets de la classe "Employee"
+		
+        Employee employee1 = new Employee("11AAA66", "Dupont", "Jean", "1991-12-10", 3500.0);
+        Employee employee2 = new Employee("33XYZ44", "Martin", "Marie", "1991-07-10", 3200.0);
+        Employee employee3 = new Employee("55LMN66", "Leroy", "Pierre", "1968-03-10", 3700.0);
+        Employee employee4 = new Employee("77QRS88", "Moreau", "Alice", "1954-12-10", 3000.0);
 
-		// TODO afficher les informations des employés avec System.out.println
-	}
+        // Affichage des informations des employés
+        // System.out.println(employee1);
+        // System.out.println(employee2);
+        // System.out.println(employee3);
+        // System.out.println(employee4);
+		
+		Employee.displayAllEmployees();
+        // Affichage des salaires nets et des jours avant les anniversaires
+        System.out.println("Salaires nets :");
+        System.out.println("N°" + employee1.getRegistrationNumber()+ " " + employee1.getFirstName() + " " + employee1.getLastName() + " (" + employee1.getBirthDate() + "): " + employee1.netSalary() + "  Jours avant anniversaire : " + employee1.daysBeforeBirthday());
+        System.out.println(employee2.getFirstName() + " " + employee2.getLastName() + " (" + employee2.getBirthDate() + "): " + employee2.netSalary() + "  - Jours avant anniversaire : " + employee2.daysBeforeBirthday());
+        System.out.println(employee3.getFirstName() + " " + employee3.getLastName() + " (" + employee3.getBirthDate() + "): " + employee3.netSalary() + " - Jours avant anniversaire : " + employee3.daysBeforeBirthday());
+        System.out.println(employee4.getFirstName() + " " + employee4.getLastName() + " (" + employee4.getBirthDate() + "): " + employee4.netSalary() + " - Jours avant anniversaire : " + employee4.daysBeforeBirthday());
+    }
 }
-
